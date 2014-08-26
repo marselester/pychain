26615cf6b904a3c8db265755bd288d6bb6ed3f23
marselester <marselester@ya.ru>
2014-08-26 22:36:36 +0800

    Add WebhookEvent API resource

--- chain/__init__.py
+++ chain/__init__.py
@@ -28,3 +28,20 @@ class Webhook(object):
         if id_ is not None:
             data['id'] = id_
         return rest('post', cls._api_path, data=data)
+
+
+class WebhookEvent(object):
+    _api_path = 'webhooks/{webhook_id}/events'
+
+    @classmethod
+    def create(cls, webhook_id, event, block_chain, address, confirmations=None):
+        data = {
+            'webhook_id': webhook_id,
+            'event': event,
+            'block_chain': block_chain,
+            'address': address
+        }
+        if confirmations is not None:
+            data['confirmations'] = confirmations
+        path = cls._api_path.format(webhook_id=webhook_id)
+        return rest('post', path, data=data)
